ae49cd2517a9aa9d2eeb11036cad41d776db645b
cooldye <cooldye@gmail.com>
2019-08-26 14:28:21 +0800

    Update main.go

--- main.go
+++ main.go
@@ -87,6 +87,32 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				_, err = bot.SendImage([]string{content.From}, img, img)
 				delete(food, content.From)
 			}
+			
+			for j := 0; j < 3; j++ {
+				i := 0
+				if results.Total >= 16 {
+					i = rand.Intn(16)
+				} else if results.Total >= 8 {
+					i = rand.Intn(8)
+				} else if results.Total > j {
+					i = j
+				} else if results.Total <= j && results.Total != 0 {
+					_, err = bot.SendText([]string{content.From}, "已無更多資料！")
+					break
+				}
+				urlOrig := UrlShortener{}
+				urlOrig.short(results.Businesses[i].MobileURL)
+				address := strings.Join(results.Businesses[i].Location.DisplayAddress, ",")
+				var largeImageURL = strings.Replace(results.Businesses[i].ImageURL, "ms.jpg", "l.jpg", 1)
+
+				_, err = bot.SendImage([]string{content.From}, largeImageURL, largeImageURL)
+				_, err = bot.SendText([]string{content.From}, "店名："+results.Businesses[i].Name+"\n電話："+results.Businesses[i].Phone+"\n評比："+strconv.FormatFloat(float64(results.Businesses[i].Rating), 'f', 1, 64)+"\n更多資訊："+urlOrig.ShortUrl)
+				_, err = bot.SendLocation([]string{content.From}, results.Businesses[i].Name+"\n", address, float64(results.Businesses[i].Location.Coordinate.Latitude), float64(results.Businesses[i].Location.Coordinate.Longitude))
+			}
+			_, err = bot.SendText([]string{content.From}, "請問你想吃什麼?\nex:義大利麵\n\n想不到吃什麼，也可以直接'傳送目前位置訊息'\nex：")
+			var img = "http://imageshack.com/a/img921/318/DC21al.png"
+			_, err = bot.SendImage([]string{content.From}, img, img)
+			delete(food, content.From)
 		}
 	}
 	
