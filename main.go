9c3f0f0b09192404d1397a34e050d98c7da641e7
cooldye <cooldye@gmail.com>
2019-11-04 15:25:38 +0800

    Update main.go

--- main.go
+++ main.go
@@ -76,9 +76,8 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					out := fmt.Sprintf("HELP")
 					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(out)).Do()
 				} else if strings.Contains(message.Text, "吃飯"){
-					originalContentURL := "https://www.foodpanda.com.tw/?gclid=EAIaIQobChMIkPvY7oLQ5QIVCGoqCh3N7QKGEAAYASAAEgJ8WPD_BwE"
-					previewImageURL := "https://www.foodpanda.com.tw/?gclid=EAIaIQobChMIkPvY7oLQ5QIVCGoqCh3N7QKGEAAYASAAEgJ8WPD_BwE"
-					bot.ReplyMessage(event.ReplyToken, linebot.NewImageMessage(originalContentURL, previewImageURL)).Do()
+					out := fmt.Sprintf("https://www.foodpanda.com.tw/?gclid=EAIaIQobChMIkPvY7oLQ5QIVCGoqCh3N7QKGEAAYASAAEgJ8WPD_BwE")
+					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(out)).Do()
 				} else if strings.Contains(message.Text, "吃"){
 					rand.Seed(time.Now().UnixNano()) // Try changing this number!
 					answers := []string{
